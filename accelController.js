526cf0da4a2ac6c4dd376b4d21f279ea0c5355f3
phuong <phuonngng1008@gmail.com>
2019-07-27 09:14:46 +0700

    --

--- accelController.js
+++ accelController.js
@@ -1,10 +1,7 @@
-let Accel = require('./accelModel');
-let mongoose = require('mongoose');
+
 const MongoClient = require('mongodb').MongoClient;
 
 const uri = "mongodb+srv://phuong01:12345@fptclus-vezuu.gcp.mongodb.net/test?retryWrites=true&w=majority";
-mongoose.connect(uri,{useNewUrlParser:true});
-
 
 
 exports.index = function (req, res) {
