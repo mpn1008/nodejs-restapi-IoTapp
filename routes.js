65d3f82032d08df698726e97de39c664b4be39f0
phuong <phuonngng1008@gmail.com>
2019-07-27 11:15:39 +0700

    p

--- routes.js
+++ routes.js
@@ -1,9 +1,8 @@
 let router = require('express').Router();
-// Set default API response
+
 router.get('/', function (req, res) {
     res.json({
-        status: 'API Its Working',
-        message: 'Welcome to RESTHub crafted with love!',
+        status: 'API is Working'
     });
 });
 let aCtrl = require('./accelController');
