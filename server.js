371681b593dcaa1b89e846a8cac203eebe28bfd1
phuong <phuonngng1008@gmail.com>
2019-07-27 09:45:42 +0700

    p

--- server.js
+++ server.js
@@ -7,25 +7,9 @@ let routes = require('./routes');
 app.use(bodyParse.urlencoded({extended:true}));
 app.use(bodyParse.json());
 
-
-/*const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("unipj").collection("accel");
-  console.log('connected');
-  var ins = {axisx:1 , axisy: 2, axisz: 32};
-  collection.insertOne(ins, function(err,res){
-      console.log('inserted a new obj');
-  })
-
-  client.close();
-});
-*/
-
-
 app.get('/', (req, res) => res.send('Hello World with Express'));
 app.use('/api', routes);
-// Launch app to listen to specified port
+
 app.listen(port, function () {
-    console.log("Running RestHub on port " + port);
+    console.log("Running Rest webservice on port " + port);
 });
-// Launch app to listen to specified port
