7157c6664d1b617820ff6410ebf31de439009126
phuong <phuonngng1008@gmail.com>
2019-08-28 09:23:46 +0700

    fds

--- accelController.js
+++ accelController.js
@@ -5,20 +5,52 @@ var async = require("async");
 const uri = "mongodb+srv://phuong01:12345@fptclus-vezuu.gcp.mongodb.net/test?retryWrites=true&w=majority";
 
 //GET all elements from the database
-exports.index = async function (req, res) {
+/*exports.index = async function (req, res) {
     const client = new MongoClient(uri, { useNewUrlParser: true });
     client.connect(err => {
         const collection = client.db("unipj").collection("sensordatas");
         console.log('connected');
-        await collection.find({}).toArray(function(err, result) {
+        collection.find({}).toArray(function(err, result) {
             if (err) throw err;
             console.log(result);
             res.json(result);
         
         });
-        await client.close();
+       client.close();
       });
+}*/
+async function find() {
+
+  const client = await MongoClient.connect(url, { useNewUrlParser: true })
+      .catch(err => { console.log(err); });
+
+  if (!client) {
+      return;
+  }
+
+  try {
+
+      const db = client.db("unipj");
+
+      let collection = db.collection('sensordatas');
+
+      let res =  await collection.find({}).toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.json(result);
+    });
+
+      console.log(res);
+
+  } catch (err) {
+
+      console.log(err);
+  } finally {
+
+      client.close();
+  }
 }
+exports.index = find();
 
 exports.new = function(req, res){
     const client = new MongoClient(uri, { useNewUrlParser: true });
