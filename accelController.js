7e509832914d64ef016f8c2a93646fc642937821
phuong <phuonngng1008@gmail.com>
2019-08-28 09:41:25 +0700

    dsa

--- accelController.js
+++ accelController.js
@@ -19,35 +19,21 @@ const uri = "mongodb+srv://phuong01:12345@fptclus-vezuu.gcp.mongodb.net/test?ret
        client.close();
       });
 }*/
-exports.index = async function(req,res) {
-
-  const client = await MongoClient.connect(url, { useNewUrlParser: true })
-      .catch(err => { console.log(err); });
-
-  if (!client) {
-      return;
-  }
-
-  try {
-
-      const db = client.db("unipj");
-
-      let collection = db.collection('sensordatas');
-
-      let res =  await collection.find({}).toArray(function(err, result) {
-        if (err) throw err;
-        console.log(result);
-        res.json(result);
-    });
-      console.log(res);
-
-  } catch (err) {
-      console.log(err);
-  } finally {
+module.exports = {
+  myFunction: async (query) => {
+    let db, client;
+    try {
+      client = await MongoClient.connect(uri, { useNewUrlParser: true });
+      db = client.db('unipj');
+      return await db.collection('sensordatas').find({}).toArray();
+    } finally {
       client.close();
+    }
   }
 }
-
+exports.index = function(req , res){
+  res = myFunction();
+}
 
 exports.new = function(req, res){
     const client = new MongoClient(uri, { useNewUrlParser: true });
